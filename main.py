d44d4f32e9470a4a6b57a4ea14d92b0eb978187f
Daniel Monteagudo <dmonte3210@gmail.com>
2018-01-27 15:32:15 -0500

    i believe i have done a thing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request
 from utils import dbhelper as db
 
 #Send noots
@@ -7,9 +7,20 @@ app = Flask(__name__)
 @app.route("/")
 def mainpage():
     return render_template("index.html")
-    # id = db.getNewId("ACCOUNTS")
-    # db.execQuery("INSERT INTO ACCOUNTS VALUES (?,?,?,?)", (id, "dog" + str(id), "AAAA", 0))
-    # return str(db.getNewId("ACCOUNTS"))
+
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+    elif request.method == 'POST':
+        uname = request.args.get("username")
+        hashpw = hash(request.args.get("password"))
+        return db.checkLogin(uname, hashpw)
+
+@app.route("/signup")
+def signup():
+    return "hi"
+
 
 @app.route("/showaccounts")
 def accounts():
