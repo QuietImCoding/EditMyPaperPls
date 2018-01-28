a0f7b010bb4d314f7304219809d526e166bc6dc0
Daniel Monteagudo <dmonte3210@gmail.com>
2018-01-28 00:38:21 -0500

    javascript work kn prograess

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,3 +1,27 @@
 editing = false;
+var start;
+var end;
 
-window.
+var content = document.getElementById("contents");
+
+document.getElementById("edit").onclick = function(e) {
+    editing = !editing;
+    if (editing) {
+        var ta = document.createElement("TEXTAREA");
+        ta.value = content.innerHTML;
+        var numlines = ta.value.split("\n").length;
+        ta.onselect = function(e) {
+            start = e.originalTarget.selectionStart;
+            end = e.originalTarget.selectionEnd;
+            console.log(ta.value.substring(start, end))
+
+        }
+        content.innerHTML = "";
+        ta.setAttribute("rows", numlines);
+        ta.style.border = "none";
+        content.appendChild(ta);
+    } else {
+        content.innerHTML = content.firstChild.value;
+    }
+}
+
